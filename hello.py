58e61a81700481add537574ceb475afce2a9dd11
Yash Sharma <37216644+sharma18yash@users.noreply.github.com>
2021-11-27 10:59:52 +0530

    Added nlp capability

--- hello.py
+++ hello.py
@@ -2,16 +2,51 @@ from flask import Flask, redirect, url_for, request
 # from flask.templating import render_template
 from flask import Flask, render_template, request
 from pymongo import MongoClient
-
+import spacy
+import numpy
+from word2number import w2n
+nlp = spacy.load("en_core_web_sm")
+from autocorrect import Speller
+from spacy.vocab import Vocab
+vocab = Vocab()
 
 connection=MongoClient()
+#db=connection['conf1']
+#collection=db['confall']
 db=connection['conf']
-collection=db['conf2021']
+collection=db['conf2']
+
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
 # query="test_data"
+def get_nlp(query):
+   spell = Speller(lang='en')
+   query = query.lower()
+   inc = ["paper", "conference", "show", "computer", "computing"]
+   short_forms = {'ml': "machine learning", "nlp":"natural language processing",
+                  "cn":"networks", "dbms":"database",
+                  "ai":"artificial intelligence"}
+
+
+   doc = nlp(query)
+   to_search=[]
+   for i in doc:
+      new_i = spell(i.text)
+      print(new_i, i.pos_)
+      # print(i, i.pos_, i.lemma_)
+      if(new_i in short_forms.keys()):
+         z = short_forms[new_i].split()
+         for new_i in z:
+               to_search.append(new_i)
+         continue
+
+      if((i.pos_ == "NOUN" or i.pos_=="VERB") and (i.lemma_ not in inc)):
+         to_search.append(i.text)
+      
+   return to_search
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -21,34 +56,73 @@ def hello_world():
       return redirect(url_for("index2", query=query))
    return render_template('index.html')
 
+from flask import Flask, url_for
 @app.route("/index2", methods=['GET', 'POST'])
 def index2():
    if request.method=="GET":
       query = request.args.get('query', None) 
+      recentQ = []
       print(query)
-      collection2=collection.find({ }).sort("Rank")
+      recentQ.append(query)
       output = []
-      for i in collection2:
-         title = i["Title"]
-         Acronym = i["Acronym"]
-         Source = i["Source"]
-         Rank = i["Rank"]
-         hasData = i["hasData"]
-         PrimaryFor = i["Primaryfor"]
-         url_for = i["url_for"]
-         if query.lower() in title.lower():
-            d = {}
-            d["Title"] = title
-            d["Acronym"] = Acronym
-            d["Source"] = Source
-            d["Rank"] = Rank
-            d["hasData"] = hasData
-            d["Primaryfor"] = PrimaryFor
-            d["url_for"] = url_for
-            output.append(d)
+      queries = get_nlp(query)
+      for query in queries:
+         for i in collection.find():
+            title = i["Title"]
+            try:
+               Acronym = i["Acronym"]
+            except KeyError:
+               Acronym = ""
+            try:
+               Source = i["Source"]
+            except KeyError:
+               Source = ""
+            try:
+               Rank = i["Rank"]
+            except KeyError:
+               Rank = ""
+            # try:
+            #    hasData = i["hasData"]
+            # except KeyError:
+            #    hasData = ""
+            # try:
+            #    PrimaryFor = i["Primaryfor"]
+            # except KeyError:
+               # PrimaryFor = ""
+            try:
+               startDate = i["Start_Date"]
+            except:
+               startDate = ""
+            try:
+               endDate = i["End_Date"]
+            except:
+               endDate=""
+            try:
+               location = i["Location"]
+            except:
+               location="" 
+            try:
+               url_for = i["url_for"]
+            except KeyError:
+               url_for = ""
+            if query.lower() in title.lower():
+               d = {}
+               d["Title"] = title
+               d["Acronym"] = Acronym
+               d["Source"] = Source
+               d["Rank"] = Rank
+               d["Start_Date"] = startDate
+               d["End_Date"] = endDate
+               d["Location"] = location
+               d["url_for"] = url_for
+               output.append(d)
+         print(output)
+      return render_template('index2.html', output=output, recentQ = recentQ)
+   if request.method=="POST":
+      query1 = request.form['sField']
+      print(query1)
 
-      print(output)
-      return render_template('index2.html', output=output)
+
 
 if __name__ == '__main__':
-   app.run()
+   app.run(port=8000,debug=True)
